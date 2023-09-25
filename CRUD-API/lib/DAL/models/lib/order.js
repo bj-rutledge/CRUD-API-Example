1b087fee6d9ac673bf976515fa7dc47e4d8ab3df
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2023-09-25 14:50:01 -0700

    add order and product model. move customer and employee model to seperate files. edit dal models to export all models. (#21)
    Added schemas. Still need to write tests. Create new issue for tests

---
+++ CRUD-API/lib/DAL/models/lib/order.js
@@ -0,0 +1,33 @@
+'use strict'; 
+
+/**Created by BJ Rutledge 
+ * 2023-09-25
+ * License MIT
+ */
+
+const {Schema, model} = require('mongoose');
+const Product  = require('../../../product');
+
+const schema = {
+   id: {
+      type: Number, 
+      required: [true, 'Must have an order number.']
+   },
+   date: {
+      type: Date, 
+      required: [true, 'Must have a date of order'],
+   },
+   items: {
+      type: [Product],
+      required: [true, 'Must include an array of products'],
+      validate: [(arry) => arry.length > 0, 'Must have at least one product in order.']
+   }
+
+}
+
+
+const orderSchema = new Schema(schema);
+
+const orderModel = model('Order', orderSchema); 
+
+module.exports = orderModel; 
