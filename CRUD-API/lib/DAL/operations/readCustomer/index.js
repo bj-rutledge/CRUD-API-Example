80cb4f40e49dd37025c2bb285531ffc37a7e1aa9
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2023-12-03 09:16:21 -0800

    added todos to readCustomer  commit

--- CRUD-API/lib/DAL/operations/readCustomer/index.js
+++ CRUD-API/lib/DAL/operations/readCustomer/index.js
@@ -4,12 +4,13 @@
  * Date:2023-11-26
  **/
 
-const { CustomerModel } = require('../../db');
-
+const {CustomerModel} = require('../../db'); 
+//todo implement error handling 
+//todo need to add logic that checks query before sending. Only want to query customer number, email, and maybe order number  
 const readCustomer = (query) => {
    console.debug('Attempting to retrieve custoemr record.');
    const customer = CustomerModel.findOne(query);
-   return customer;
-};
+   return customer; 
+}
 
-module.exports = readCustomer;
+module.exports = readCustomer; 
