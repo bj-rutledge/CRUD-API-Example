8dff640e5aac20fc8ee3322608ca0debd5cd77f0
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2024-11-08 15:02:58 -0800

    helper to validate incomming customer queries for routes. Update customer routes for validation.

--- CRUD-API/routes/customer/index.js
+++ CRUD-API/routes/customer/index.js
@@ -2,19 +2,16 @@
  * Created by BJ Rutledge
  * Date:2024-02-21
  **/
-
+//todo set up logger for routes 
 const customerRouter = require('express').Router();
 const { readCustomer, updateCustomer } = require('../../lib/DAL/operations');
+const isValidIncommingCustomerQuery = require('../helpers/isValidIncommingCustomerQuery');
 
 customerRouter.get('/get-customer', (req, res, next) => {
    const query = req.query;
-
+   
    console.debug('Query received:', query);
-   if (
-      query.email == undefined &&
-      query.phoneNumber == undefined &&
-      query.customerNumber == undefined
-   ) {
+   if (!isValidIncommingCustomerQuery(query)) {
       res.status(400).send(
          'Query Error. Invalid query. Must query with email, phone, or customer number.'
       );
@@ -45,7 +42,7 @@ customerRouter.put('/update-customer', (req, res, next) => {
    //receiving a json body with filter and update params
    const query = req.body;
    //short circuit validation.
-   if (query.update == undefined || query.filter == undefined) {
+   if (query.update == undefined || query.filter == undefined || !isValidIncommingCustomerQuery(query.filter)) {
       res.status(400).send(
          `Invalid query params. Must send filter and update. query.filter: ${query.filter} query.update: ${query.update}`
       );
